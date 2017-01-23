98b65085e2da9e3295600e3e374bf8a16e4dea81
afbdev <afbdev@alexanders-mbp.attlocal.net>
2017-01-22 18:29:07 -0600

    Added login view and keyboard observing

---
+++ LoginGuide/LoginCell.swift
@@ -0,0 +1,85 @@
+//
+//  LoginCell.swift
+//  LoginGuide
+//
+//  Created by afbdev on 1/22/17.
+//  Copyright © 2017 afbdev. All rights reserved.
+//
+
+import UIKit
+
+class LoginCell: UICollectionViewCell {
+    
+    let logoImageView: UIImageView = {
+        let image = UIImage(named: "logo")
+        let imageView = UIImageView(image: image)
+        return imageView
+    }()
+    
+    let emailTextField: LeftPaddedTextField = {
+        let tf = LeftPaddedTextField()
+        tf.placeholder = "Enter email"
+        tf.layer.borderColor = UIColor.lightGray.cgColor
+        tf.layer.borderWidth = 0.5
+        tf.keyboardType = .emailAddress
+        return tf
+    }()
+    let passwordTextField: LeftPaddedTextField = {
+        let tf = LeftPaddedTextField()
+        tf.placeholder = "Enter password"
+        tf.layer.borderColor = UIColor.lightGray.cgColor
+        tf.layer.borderWidth = 0.5
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.backgroundColor = UIColor(red: 247/255, green: 154/255, blue: 27/255, alpha: 1)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(logoImageView)
+        addSubview(emailTextField)
+        addSubview(passwordTextField)
+        addSubview(loginButton)
+        
+        _ = logoImageView.anchor(centerYAnchor, left: nil, bottom: nil, right: nil, topConstant: -230, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 160, heightConstant: 160)
+        logoImageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+        
+        _ = emailTextField.anchor(logoImageView.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, topConstant: 8, leftConstant: 32, bottomConstant: 0, rightConstant: -32, widthConstant: 0, heightConstant: 50)
+        
+        _ = passwordTextField.anchor(emailTextField.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, topConstant: 8, leftConstant: 32, bottomConstant: 0, rightConstant: -32, widthConstant: 0, heightConstant: 50)
+        
+        _ = loginButton.anchor(passwordTextField.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, topConstant: 8, leftConstant: 32, bottomConstant: 0, rightConstant: -32, widthConstant: 0, heightConstant: 50)
+        
+    }
+    
+    
+    
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+class LeftPaddedTextField: UITextField {
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: bounds.origin.x + 10, y: bounds.origin.y, width: bounds.width + 10, height: bounds.height)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: bounds.origin.x + 10, y: bounds.origin.y, width: bounds.width + 10, height: bounds.height)
+
+    }
+}
+
+
+
